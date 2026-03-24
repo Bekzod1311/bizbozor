502f7ba822cdeea01617f962e23932454b0d245a
Bekzod <bekzod.khusanov@internet.ru>
2026-03-24 23:15:06 +0500

    Improved create listing form UI

--- listings/forms.py
+++ listings/forms.py
@@ -1,24 +1,20 @@
 from django import forms
 from .models import Listing
 
-
 class ListingForm(forms.ModelForm):
-    """
-    User e'lon qo'shishi uchun form.
-    """
-
     class Meta:
         model = Listing
-        fields = [
-            'category',
-            'region',
-            'district',
-            'title',
-            'price',
-            'short_description',
-            'description',
-            'phone',
-            'telegram_username',
-            'google_maps_link',
-            'main_image',
-        ]
+        fields = '__all__'
+
+        widgets = {
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'region': forms.Select(attrs={'class': 'form-control'}),
+            'district': forms.Select(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'short_description': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'telegram_username': forms.TextInput(attrs={'class': 'form-control'}),
+            'google_maps_link': forms.URLInput(attrs={'class': 'form-control'}),
+        }
